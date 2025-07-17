85d94479679a31bc0325dfda787bc7ac532636f0
victorvd98 <victor.van.damme@icloud.com>
2025-07-17 21:10:32 +0200

    added logic and dom modules to index file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./styles.css";
 
+//this is the index file that imports both the logic and DOM manipulation files
+//logic = handling tasks
+//DOM manipulation = updating the UI to reflect the logic
+import "./logic.js";
+import "./dom.js";
+
