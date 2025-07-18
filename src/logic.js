ffc08e9632240ff2961a5a7840494e72c1fa37bc
victorvd98 <victor.van.damme@icloud.com>
2025-07-18 17:48:45 +0200

    dd

--- src/logic.js
+++ src/logic.js
@@ -1,3 +1,5 @@
+import { format, isValid } from "date-fns";
+
 //ES6 class syntax
 class List {
     constructor() {
@@ -5,8 +7,10 @@ class List {
     }
     create(task) {
         const todo = new Task(task);
-        this.todos.append(todo);
+        this.todos.push(todo);
         console.log(this.todos);
+        console.log(todo.dateAdded);
+        return null;
     }
 }
 
@@ -14,8 +18,24 @@ class Task {
     constructor(title) {
         this.title = title;
         this.done = false;
-        this.dateAdded = 0; //wanna use date-fns for this
-        this.dueDate = 0; //for now...
+        this.dateAdded = format(Date.now(), 'yyyy-MM-dd HH:mm'); //JavaScript native Date class, date-fns format method to cleanup
+        this._dueDate = null; //we set dueDates later (some tasks may not have due dates)
+    }
+    //methods
+    toggleDone() {
+        if (this.done === false) {
+            this.done = true;
+        } else {
+            this.done = false;
+        }
+    }
+    set dueDate(dateInput) {
+        if (isValid(dateInput)) {
+            let date = format(new Date(dateInput));
+            this._dueDate = date;
+        } else {
+            console.log('Sorry thats invalid');
+        }
     }
 }
 
