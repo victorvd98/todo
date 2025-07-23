2f9c18f5431b8eba87f2f025ab4e898e40285550
victorvd98 <victor.van.damme@icloud.com>
2025-07-23 10:13:33 +0200

    prettier formatting

--- src/dom.js
+++ src/dom.js
@@ -1,51 +1,49 @@
-import { list, saveToStorage, loadFromStorage } from './logic.js';
+import { list, saveToStorage, loadFromStorage } from "./logic.js";
 
+export function renderTask(task) {
+  //you can do stuff simultaneously apparently
+  const taskList = document.getElementById("task-list");
 
-export function renderTask(task) { //you can do stuff simultaneously apparently
-    const taskList = document.getElementById("task-list");
+  const taskDiv = document.createElement("div");
+  taskDiv.id = `${task.title}`;
+  taskDiv.classList.add("task");
 
-    const taskDiv = document.createElement('div');
-    taskDiv.id = `${task.title}`;
-    taskDiv.classList.add('task');
+  //Title
+  const title = document.createElement("span");
+  title.textContent = task.title;
+  title.classList.add("task-title");
 
+  //Due Date (if exists)
+  const due = document.createElement("span");
+  due.classList.add("task-due");
+  due.textContent = task.dueDate ? `Due: ${task.dueDate}` : "";
 
-    //Title
-    const title = document.createElement('span');
-    title.textContent = task.title;
-    title.classList.add("task-title");
+  // Done checkbox
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = task.done;
+  checkbox.addEventListener("change", () => {
+    task.toggleDone();
+    taskDiv.classList.toggle("done", task.done); //boolean adds or removes the class based on value
+    saveToStorage();
+  });
 
-    //Due Date (if exists)
-    const due = document.createElement('span');
-    due.classList.add("task-due");
-    due.textContent = task.dueDate ? `Due: ${task.dueDate}` : ""
+  //Delete Button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "🗑️";
+  deleteBtn.classList.add("delete-button");
 
-    // Done checkbox
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.checked = task.done;
-    checkbox.addEventListener('change', () => {
-        task.toggleDone();
-        taskDiv.classList.toggle('done', task.done); //boolean adds or removes the class based on value
-        saveToStorage();
-    });
+  deleteBtn.addEventListener("click", () => {
+    list.todos = list.todos.filter((t) => t !== task);
+    taskDiv.remove(); // removes the task from the DOM
+    saveToStorage();
+  });
 
-    //Delete Button
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = '🗑️';
-    deleteBtn.classList.add('delete-button');
-
-    deleteBtn.addEventListener('click', () => {
-        list.todos = list.todos.filter(t => t !== task);
-        taskDiv.remove(); // removes the task from the DOM
-        saveToStorage();
-    });
-
-    //Assembly of task card
-    taskDiv.appendChild(title);
-    taskDiv.appendChild(due);
-    taskDiv.appendChild(checkbox);
-    taskDiv.appendChild(deleteBtn);
-    //Add task card to task list
-    taskList.appendChild(taskDiv);
+  //Assembly of task card
+  taskDiv.appendChild(title);
+  taskDiv.appendChild(due);
+  taskDiv.appendChild(checkbox);
+  taskDiv.appendChild(deleteBtn);
+  //Add task card to task list
+  taskList.appendChild(taskDiv);
 }
-
--- src/logic.js
+++ src/logic.js
@@ -2,48 +2,48 @@ import { format, isValid } from "date-fns";
 
 //ES6 class syntax
 class List {
-    constructor() {
-        this.todos = [];
-    }
-    create(task) {
-        const todo = new Task(task);
-        this.todos.push(todo);
-        saveToStorage();
-        return null;
-    }
+  constructor() {
+    this.todos = [];
+  }
+  create(task) {
+    const todo = new Task(task);
+    this.todos.push(todo);
+    saveToStorage();
+    return null;
+  }
 }
 
 class Task {
-    constructor(title) {
-        this.title = title;
-        this.done = false;
-        this.dateAdded = format(Date.now(), 'yyyy-MM-dd HH:mm'); //JavaScript native Date class, date-fns format method to cleanup
-        this._dueDate = null; //we set dueDates later (some tasks may not have due dates)
+  constructor(title) {
+    this.title = title;
+    this.done = false;
+    this.dateAdded = format(Date.now(), "yyyy-MM-dd HH:mm"); //JavaScript native Date class, date-fns format method to cleanup
+    this._dueDate = null; //we set dueDates later (some tasks may not have due dates)
+  }
+  //methods
+  toggleDone() {
+    if (this.done === false) {
+      this.done = true;
+    } else {
+      this.done = false;
     }
-    //methods
-    toggleDone() {
-        if (this.done === false) {
-            this.done = true;
-        } else {
-            this.done = false;
-        }
-    }
+  }
 
-    get dueDate() {
-        return this._dueDate;
-    }
+  get dueDate() {
+    return this._dueDate;
+  }
 
-    set dueDate(dateInput) {
-        if (isValid(dateInput)) {
-            let date = format(new Date(dateInput), 'yyyy-MM-dd'); //no HH:mm on due dates
-            this._dueDate = date;
-        } else {
-            console.log('Sorry thats invalid');
-        }
+  set dueDate(dateInput) {
+    if (isValid(dateInput)) {
+      let date = format(new Date(dateInput), "yyyy-MM-dd"); //no HH:mm on due dates
+      this._dueDate = date;
+    } else {
+      console.log("Sorry thats invalid");
     }
-    editTitle(newTitle) {
-        this.title = newTitle;
-    }
+  }
+  editTitle(newTitle) {
+    this.title = newTitle;
+  }
 }
 
 //creation of the Task List
@@ -59,7 +59,7 @@ export function loadFromStorage() {
   const stored = JSON.parse(localStorage.getItem("todos"));
   if (!stored) return;
 
-  stored.forEach(obj => {
+  stored.forEach((obj) => {
     const task = new Task(obj.title);
     task.done = obj.done;
     task._dueDate = obj._dueDate;
@@ -67,4 +67,3 @@ export function loadFromStorage() {
     list.todos.push(task);
   });
 }
-
